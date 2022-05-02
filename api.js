3a6a1c6bde37c24b747bc8864397c416b29a49b6
cpprogues <joshgarde@gmail.com>
2022-05-02 16:35:41 -0700

    initial commit

---
+++ api.js
@@ -0,0 +1,100 @@
+const {parentPort} = require('worker_threads');
+const express = require('express');
+const pigpio = require('js-pigpio');
+
+const PORT = 8080;
+const MAX_DURATION = 15 * 60 * 1000;
+
+const app = express();
+const pi = new pigpio();
+
+var heaterEnabled = false;
+var timerEnabled = false;
+var timerStart = Date.now();
+var timerEnd = Date.now();
+
+app.use(express.json());
+
+app.get('/status', function (req, res) {
+  res.json({
+    heater: heaterEnabled,
+    timer: {
+      enabled: timerEnabled,
+      start: timerStart,
+      end: timerEnd
+    }
+  });
+});
+
+app.post('/timer', function (req, res) {
+  let {enable, duration} = req.body.enable;
+
+  if (typeof(enable) !== 'boolean')
+    return res.status(400).json({error: 'enable is not a boolean'});
+
+  if (typeof(duration) !== 'number' && !isNaN(duration))
+    return res.status(400).json({error: 'duration is not a valid number'});
+
+  duration = Math.floor(duration);
+  if (duration > MAX_DURATION)
+    return res.status(400).json({error: 'duration is too high'});
+
+  if (heaterEnabled === true && enable === true)
+    return res.status(400).json({error: 'Can not start another timer while one is active'});
+
+  if (enable === false) {
+    timerEnabled = false;
+    timerEnd = Date.now();
+
+    pi.write(17, 0, (level) => {
+      heaterEnabled = level === 1;
+
+      res.json({
+        heater: heaterEnabled,
+        timer: {
+          enabled: timerEnabled,
+          start: timerStart,
+          end: timerEnd
+        }
+      });
+    });
+  } else {
+    timerEnabled = true;
+    timerStart = Date.now();
+    timerEnd = Date.now() + duration;
+
+    pi.write(17, 1, (level) => {
+      heaterEnabled = level === 1;
+
+      res.json({
+        heater: heaterEnabled,
+        timer: {
+          enabled: timerEnabled,
+          start: timerStart,
+          end: timerEnd
+        }
+      });
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`[API] Listening on 0.0.0.0:${PORT}`);
+  pi.set_mode(17, pi.OUTPUT);
+  pi.write(17, 0);
+
+  setInterval(timerWorker, 50);
+});
+
+function timerWorker() {
+  if (timerEnabled && Date.now() >= timerEnd) {
+    timerEnabled = false;
+    pi.write(17, 0, (level) => {
+      heaterEnabled = level === 1;
+    });
+  }
+}
+
+parentPort.on('message', (nonce) => {
+  parentPort.postMessage(nonce);
+});
---
+++ main.js
@@ -0,0 +1,66 @@
+const crypto = require('crypto');
+const {Worker} = require('worker_threads');
+
+const pigpio = require('js-pigpio');
+
+const MAX_RESTARTS = 5;
+const MAX_TIMEOUT = 10 * 1000;
+
+function main() {
+  let restarts = 0;
+
+  function onError() {
+    // ESTOP
+    const pi = new pigpio();
+    pi.set_mode(17, pi.OUTPUT);
+    pi.write(17, 0);
+
+    if (restarts >= MAX_RESTARTS) return;
+
+    restarts++;
+    console.log(`[Watchdog] Attemping restart... (${restarts}/${MAX_RESTARTS})`);
+    startWatchdog(onError);
+  }
+
+  startWatchdog(onError);
+}
+
+function startWatchdog(onError) {
+  let lastPong = Date.now();
+  let nonce;
+
+  let worker = new Worker('./api.js');
+
+  worker.on('error', (err) => {
+    console.log(`[Watchdog] Error thrown: ${err}`);
+    clearInterval(timer);
+    onError();
+  });
+  worker.on('exit', (exitCode) => {
+    console.log(`[Watchdog] Thread exited with code: ${exitCode}`);
+    clearInterval(timer);
+    onError();
+  });
+  worker.on('message', (recvNonce) => {
+    recvNonce = Buffer.from(recvNonce);
+
+    if (!nonce.equals(recvNonce)) {
+      console.log('[Watchdog] Nonce mismatch triggered');
+      worker.terminate();
+    } else {
+      lastPong = Date.now();
+    }
+  });
+
+  let timer = setInterval(() => {
+    if ((Date.now() - lastPong) > MAX_TIMEOUT) {
+      console.log('[Watchdog] Timeout triggered')
+      worker.terminate();
+    } else {
+      nonce = crypto.randomBytes(10);
+      worker.postMessage(nonce);
+    }
+  }, 2 * 1000);
+}
+
+main();
